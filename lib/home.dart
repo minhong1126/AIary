9ae63f4a9a654023269233a014a5e3412fe9f61c
minhong1126 <mjhong159357@gmail.com>
2024-08-08 14:37:29 +0900

    home - Appbar과 로고 추가 및 조정

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 
+const lightGreen = Color(0xffC9DABF);
+const mainGreen = Color(0xff9CA986);
+const darkGreen = Color(0xff808D7C);
+const darkestGreen = Color(0xff5F6F65);
+
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          // backgroundColor: lightGreen,
+          backgroundColor: lightGreen,
           body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          // Image.asset('assets/images/logo.png'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('logo', style: TextStyle()),
+              AppBar(
+                backgroundColor: lightGreen,
+                centerTitle: true,
+                title: Image.asset(
+                  'assets/images/logo.png',
+                  fit: BoxFit.contain,
+                  height: 30,
+                  width: 30,
+                ),
+              )
             ],
-          )
-        ],
-      )),
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:aiary/home.dart';
 import 'package:aiary/chat.dart';
 
-const lightGreen = Color(0xffC9DABF);
-const mainGreen = Color(0xff9CA986);
-const darkGreen = Color(0xff808D7C);
-const darkestGreen = Color(0xff5F6F65);
-
 void main() {
   runApp(MyApp());
 }
