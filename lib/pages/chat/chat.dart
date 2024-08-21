d4e901c6b987e12efa61c80ece8efe9bbfe73175
minhong1126 <mjhong159357@gmail.com>
2024-08-21 16:35:33 +0900

    chat - 기본 레이아웃 구현

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:aiary/theme/color.dart';
+import 'package:flutter/widgets.dart';
 
 class Chat extends StatelessWidget {
   const Chat({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: color.mainGreen,
+        body: Column(
+          children: [
+            appbar(),
+            // 채팅 화면,
+            // 채팅 입력창
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class appbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: color.darkGreen,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        color: color.white,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
