b6ce18474aebd81ee85670d3310e97ccaff01a0f
minhong1126 <mjhong159357@gmail.com>
2024-09-13 01:13:10 +0900

    chat - 입력창 생성

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:aiary/theme/color.dart';
-// import './chatlist.dart';
 
 class Chat extends StatelessWidget {
   const Chat({super.key});
@@ -13,9 +12,8 @@ class Chat extends StatelessWidget {
       appBar: _Appbar(),
       body: Column(
         children: [
-          // Expanded(
-          // child: ),
-          // _BottomInputField(),
+          Expanded(child: Container()),
+          const ChatField(),
         ],
       ),
     );
@@ -39,4 +37,35 @@ class _Appbar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class ChatField extends StatelessWidget {
+  const ChatField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color.white,
+      padding: const EdgeInsets.all(0.8),
+      child: Row(
+        children: [
+          const Expanded(
+            child: TextField(
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(border: InputBorder.none),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.send, color: color.darkGreen),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Message sent!')),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
